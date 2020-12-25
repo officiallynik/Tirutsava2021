a861ee73950e5fbf8a5777b14fd9f4be23a323ba
suryasiriki4 <cs18b035@iittp.ac.in>
2020-12-25 11:38:41 +0530

    added legacy of tirutsava page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 
+import Legacy from './pages/previous_tirutsavas/TirutsavaLegacy';
+
 import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <Legacy />
     </Router>
   );
 }
