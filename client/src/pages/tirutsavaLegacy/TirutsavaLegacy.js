021ca086c4cc5b3e31d5b2854e557b8b3967b048
suryasiriki4 <cs18b035@iittp.ac.in>
2020-12-25 12:15:18 +0530

    added tirutsavaLegacy folder

---
+++ client/src/pages/tirutsavaLegacy/TirutsavaLegacy.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+import Navbar from "../../components/navbar/Navbar";
+import YearsBar from "./YearsBar";
+
+import "./TirutsavaLegacy.css";
+
+class TirutsavaLegacy extends Component {
+    render() {
+        return(
+            <div className="triutsavaLegacy">
+
+                {/* Years bar */}
+                <YearsBar />
+
+                {/* stats of that year */}
+
+                {/* video and content */}
+
+            </div>
+        )
+    }
+}
+
+export default TirutsavaLegacy;
