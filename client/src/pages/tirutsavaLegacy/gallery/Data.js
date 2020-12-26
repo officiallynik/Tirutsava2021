f23c04aa166d9cebfc84741e1d203ab538aa41b9
suryasiriki4 <cs18b035@iittp.ac.in>
2020-12-26 12:18:18 +0530

    added the file gallery in tirutsavaLegacy folder

---
+++ client/src/pages/tirutsavaLegacy/gallery/Data.js
@@ -0,0 +1,57 @@
+export default [
+  
+    {
+      pics: [
+        "https://cdn.vox-cdn.com/thumbor/RX04uAR-GhRdN9-en6PmkSj69iU=/0x0:6080x2546/1200x800/filters:focal(2406x699:3378x1671)/cdn.vox-cdn.com/uploads/chorus_image/image/65753797/elsainfall.0.jpg",
+        "https://vignette.wikia.nocookie.net/disney/images/9/95/Profile_-_Elsa.jpeg/revision/latest?cb=20200319054311"
+      ],
+      title: "Elsa",
+      text:
+        "Ice Queen, literally. Ghost me and I'll find you & freeze you. Just ask my sister."
+    },
+    {
+      pics: [
+        "https://media1.popsugar-assets.com/files/thumbor/8AH2numkygyXyu3DWTlrb_EwSuo/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2020/02/19/863/n/3019466/48974ba25e4d8fe6328504.89366186_MCDPRIN_EC07/i/Gemini-May-21-June-20-Tiana.jpg",
+        "https://i.pinimg.com/originals/16/5e/e3/165ee335555aaa283db4598f3228762d.gif"
+      ],
+      title: "Tiana",
+      text: "Entrepreneur. NOLA girl. Firm believer in 'You have to kiss a few frogs to find your Prince Charming'."
+    },
+    {
+      pics: [
+        "https://ohmy.disney.com/wp-content/uploads/2013/08/beauty-and-the-beast-gaston-flexing.jpg",
+        "https://vignette.wikia.nocookie.net/my-disney-villains/images/1/1e/F3861726-6329-43C5-95B0-FEF74E83C7D7-244-000000193E858348.JPEG/revision/latest?cb=20170827054129"
+      ],
+      title: "Gaston",
+      text:
+        "Here for a good time, not a long time. Eats 5 dozen eggs so I'm roughly the size of a barge."
+    },
+    {
+      pics: [
+        "https://i.pinimg.com/originals/9b/1f/7f/9b1f7fb26329f0fb1bf6cd9a65063f4f.jpg",
+        "https://i.pinimg.com/originals/04/f9/35/04f93570abf5044ff3804782a9e2119d.jpg"
+      ],
+      title: "Flynn",
+      text: "YOLO Enthusiast. Former thief, so I'll steal your heart 😉"
+    },
+    {
+      pics: [
+        "https://pbs.twimg.com/media/C8HShn0VwAArWJz.jpg",
+        "https://i.pinimg.com/736x/95/ee/91/95ee91b6d78ef79a317ab9482e135458.jpg"
+      ],
+      title: "Shang",
+      text:
+        "Let's get down to business: Ex-military, defeated the Huns, mysterious as the dark side of the moon."
+    },
+    {
+      pics: [
+        "https://data.whicdn.com/images/120634023/original.jpg",
+        "https://medias.spotern.com/spots/w640/255/255774-1574993055.jpg"
+      ],
+      title: "Jasmine",
+      text:
+        "Cat Mom. If you have anything about Tiger King in your bio, swiping left. Not a prize to be won."
+    }
+    
+  
+  ];
