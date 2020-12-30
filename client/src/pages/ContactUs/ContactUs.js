751dd9a8c533f8709ea1341bde4de0304e332479
Mir Sameed Ali <meersameed@gmail.com>
2020-12-30 08:51:17 +0530

    Styled Team List

--- client/src/pages/ContactUs/ContactUs.js
+++ client/src/pages/ContactUs/ContactUs.js
@@ -6,55 +6,35 @@ import { MDBContainer,MDBListGroup, MDBListGroupItem, MDBRow, MDBCol } from "mdb
 
 
 class ContactUs extends React.Component {
-constructor(props) {
-super(props);
-this.state = {
-active: 0,
-sections: []
-};
-}
-
-// this.scrollSpyText = React.createRef();
-// }
-
-// componentDidMount() {
-// let sections = this.scrollSpyText.current.getElementsByTagName("h4");
-// this.setState({ sections });
-// }
-
-// handleScroll = (e) => {
-// const scrollTop = e.target.scrollTop;
-// const sections = this.state.sections;
-// const lastIndex = sections.length - 1;
-
-// for (let i = 0; i < lastIndex; i++) { if ((scrollTop> sections[i].offsetTop - 20) && (scrollTop < sections[i +
-//     1].offsetTop - 20)) { this.setState({ active: i }); } }; if (scrollTop> sections[lastIndex].offsetTop - 20)
-//     this.setState({ active: lastIndex });
-//     }
-
 render() {
   return (
     <>
       <MDBRow>
-        <MDBCol md="3">
-          <MDBListGroup  md="3">
-          <Scrollspy items={ ['section1', 'section2', 'section3','section4'] } currentClassName="active" style={{width:"30%",padding:'0%',height:'100%',position:'fixed'}}>
-            <MDBListGroupItem href="#section1">section1</MDBListGroupItem>
-            <MDBListGroupItem href="#section2">section2</MDBListGroupItem>
-            <MDBListGroupItem href="#section3">section3</MDBListGroupItem>
-            <MDBListGroupItem href="#section4">section4</MDBListGroupItem>
-            <MDBListGroupItem href="#section5">section5</MDBListGroupItem>
-            <MDBListGroupItem href="#section6">section6</MDBListGroupItem>
-            <MDBListGroupItem href="#section7">section7</MDBListGroupItem>
-            <MDBListGroupItem href="#section8">section8</MDBListGroupItem>
+        
+        <MDBCol md="5" >
+          <div className="scroll-spy-ContactUs">
+
+            <h1 className="team-heading-ContactUs">MEET THE TEAM</h1>
+            
+          <MDBListGroup style={{width:"100%"}}>
+          <Scrollspy items={ ['section1', 'section2', 'section3','section4'] } currentClassName="active-ContactUs" >
+            <MDBListGroupItem href="#section1" className="team-name-ContactUs">SPONSORSHIP</MDBListGroupItem>
+            <MDBListGroupItem href="#section2" className="team-name-ContactUs">EVENT MANAGEMENT</MDBListGroupItem>
+            <MDBListGroupItem href="#section3" className="team-name-ContactUs">MARKETING</MDBListGroupItem>
+            <MDBListGroupItem href="#section4" className="team-name-ContactUs">CREATIVE</MDBListGroupItem>
+            <MDBListGroupItem href="#section5" className="team-name-ContactUs">HOSPITALITY</MDBListGroupItem>
+            <MDBListGroupItem href="#section6" className="team-name-ContactUs">CONTENT DEVELOPMENT</MDBListGroupItem>
+            <MDBListGroupItem href="#section7" className="team-name-ContactUs">SECURITY</MDBListGroupItem>
+            
             </Scrollspy>
           </MDBListGroup>
+          </div>
         </MDBCol>
 
         <MDBCol>
             
-        <div>
-            <section  id="section1">
+        <div >
+            <div  className="image-div" id="section1">
             <h4>section1</h4>
             <p>
               Ad leggings keytar, brunch id art party dolor labore. Pitchfork yr
@@ -76,10 +56,10 @@ render() {
               aesthetic. Nihil tattooed accusamus, cred irony biodiesel keffiyeh
               artisan ullamco consequat.
             </p>
-            </section>
+            </div>
 
 
-            <section  id="section2">
+            <div  className="image-div" id="section2">
             <h4>section2</h4>
             <p>
               Veniam marfa mustache skateboard, adipisicing fugiat velit
@@ -101,9 +81,9 @@ render() {
               artisan ullamco consequat.
             </p>
 
-            </section>
+            </div>
 
-            <section  id="section3">
+            <div  className="image-div" id="section3">
             <h4 >section3</h4>
             <p>
               Occaecat commodo aliqua delectus. Fap craft beer deserunt
@@ -125,9 +105,9 @@ render() {
               artisan ullamco consequat.
             </p>
 
-            </section>
+            </div>
 
-            <section  id="section4">
+            <div  className="image-div" id="section4">
             <h4 >section4</h4>
             <p>
               Veniam marfa mustache skateboard, adipisicing fugiat velit
@@ -148,7 +128,7 @@ render() {
               aesthetic. Nihil tattooed accusamus, cred irony biodiesel keffiyeh
               artisan ullamco consequat.
             </p>
-            </section>
+            </div>
           </div>
         </MDBCol>
       </MDBRow>
