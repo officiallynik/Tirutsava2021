046984dc35dfe9fc11558ff78a74c737d5b8119c
Mir Sameed Ali <meersameed@gmail.com>
2020-12-30 09:36:51 +0530

    smooth scrolling

--- client/src/pages/ContactUs/ContactUs.js
+++ client/src/pages/ContactUs/ContactUs.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./ContactUs.css"
 import Scrollspy from 'react-scrollspy'
+import { Link, animateScroll as scroll } from "react-scroll";
 import { MDBContainer,MDBListGroup, MDBListGroupItem, MDBRow, MDBCol } from "mdbreact";
 
 
-
 class ContactUs extends React.Component {
+
+
+
+  LinkConfig={
+    spy:true,
+    activeClass:'active-ContactUs',
+    className:'team-name-ContactUs',
+    smooth:true,
+    offset:-70,
+    duration:1000,
+  }
+
 render() {
   return (
     <>
@@ -17,16 +29,26 @@ render() {
             <h1 className="team-heading-ContactUs">MEET THE TEAM</h1>
             
           <MDBListGroup style={{width:"100%"}}>
-          <Scrollspy items={ ['section1', 'section2', 'section3','section4'] } currentClassName="active-ContactUs" >
-            <MDBListGroupItem href="#section1" className="team-name-ContactUs">SPONSORSHIP</MDBListGroupItem>
-            <MDBListGroupItem href="#section2" className="team-name-ContactUs">EVENT MANAGEMENT</MDBListGroupItem>
+          
+            
+
+            <Link to="section1" {...this.LinkConfig}>SPONSORSHIP</Link>
+            <Link to="section2" {...this.LinkConfig}>MARKETING</Link>
+            <Link to="section3" {...this.LinkConfig}>EVENT MANAGEMENT</Link>
+            <Link to="section4" {...this.LinkConfig}>CREATIVE</Link>
+            <Link to="section5" {...this.LinkConfig}>HOSPITALITY</Link>
+            <Link to="section6" {...this.LinkConfig}>CONTENT DEVELOPMENT</Link>
+            <Link to="section7" {...this.LinkConfig}>SECURITY</Link>
+
+            {/* <MDBListGroupItem href="#section1" className="team-name-ContactUs">SPONSORSHIP</MDBListGroupItem> */}
+            {/* <MDBListGroupItem href="#section2" className="team-name-ContactUs">EVENT MANAGEMENT</MDBListGroupItem>
             <MDBListGroupItem href="#section3" className="team-name-ContactUs">MARKETING</MDBListGroupItem>
             <MDBListGroupItem href="#section4" className="team-name-ContactUs">CREATIVE</MDBListGroupItem>
             <MDBListGroupItem href="#section5" className="team-name-ContactUs">HOSPITALITY</MDBListGroupItem>
             <MDBListGroupItem href="#section6" className="team-name-ContactUs">CONTENT DEVELOPMENT</MDBListGroupItem>
-            <MDBListGroupItem href="#section7" className="team-name-ContactUs">SECURITY</MDBListGroupItem>
+            <MDBListGroupItem href="#section7" className="team-name-ContactUs">SECURITY</MDBListGroupItem> */}
             
-            </Scrollspy>
+           
           </MDBListGroup>
           </div>
         </MDBCol>
@@ -132,6 +154,7 @@ render() {
           </div>
         </MDBCol>
       </MDBRow>
+      <div style={{height:"30vh",width:"100%"}}></div>
       </>
     
     );
