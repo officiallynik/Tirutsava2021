a2e4e94bbc4c106a31707ae0a32335e91e86c43e
Mir Sameed Ali <meersameed@gmail.com>
2021-01-06 09:31:07 +0530

    Added Modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 
 import './App.css';
-import '@fortawesome/fontawesome-free/css/all.min.css'; import
-'bootstrap-css-only/css/bootstrap.min.css'; import
-'mdbreact/dist/css/mdb.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
 import ContactUs from './pages/ContactUs/ContactUs';
+import Registration from './components/Registration/Registration';
 
 function App() {
   return (
     <div className="App">
+      <Registration/>
       <ContactUs/>
+      
     </div>
   );
 }
--- client/src/components/Registration/Registration.js
+++ client/src/components/Registration/Registration.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "./Registration.css"
+import { MDBContainer, MDBBtn, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter } from 'mdbreact';
 
+class Registration extends Component {
+  state = {
+    modal: true
+  };
 
-const Registration=()=>{
-
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+  render() {
+    return (
+      <MDBContainer >
+        {/* BUTTON */}
+        <MDBBtn color="info" onClick={this.toggle}>Click</MDBBtn>
+        {/* MODAL */}
+        <MDBModal className="registration" isOpen={this.state.modal} toggle={this.toggle}    >
+          <MDBModalHeader toggle={this.toggle}>
+            <h5 className="signIn-Heading-registration">Sign in to Tirutsava Brand</h5>
+            </MDBModalHeader>
+          <MDBModalBody>
+            <p className="secondary-Heading-registration">Log in with your Google account to access all the goodies</p>
+          </MDBModalBody>
+          {/* <MDBModalFooter>
+            <MDBBtn color="secondary" onClick={this.toggle}>Close</MDBBtn>
+            <MDBBtn color="primary">Save changes</MDBBtn>
+          </MDBModalFooter> */}
+        </MDBModal>
+      </MDBContainer>
+    );
+  }
 }
+export default Registration;
