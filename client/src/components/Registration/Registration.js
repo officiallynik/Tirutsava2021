84b8cc0229ad0f09375fd74fb5b967ee73a84849
Mir Sameed Ali <meersameed@gmail.com>
2021-01-06 09:56:13 +0530

    Added Google Button

--- client/src/components/Registration/Registration.js
+++ client/src/components/Registration/Registration.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import GoogleButton from 'react-google-button'
 import "./Registration.css"
 import { MDBContainer, MDBBtn, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter } from 'mdbreact';
 
 class Registration extends Component {
   state = {
-    modal: true
+    modal: false
   };
 
   toggle = () => {
@@ -20,10 +21,17 @@ class Registration extends Component {
         {/* MODAL */}
         <MDBModal className="registration" isOpen={this.state.modal} toggle={this.toggle}    >
           <MDBModalHeader toggle={this.toggle}>
-            <h5 className="signIn-Heading-registration">Sign in to Tirutsava Brand</h5>
+            <h5 className="signIn-Heading-registration">Sign In to Tirutsava Brand</h5>
             </MDBModalHeader>
           <MDBModalBody>
-            <p className="secondary-Heading-registration">Log in with your Google account to access all the goodies</p>
+            <div className="modal-body-registration">
+              <p className="secondary-Heading-registration">Log in with your Google account to access all the goodies!</p>
+              <GoogleButton
+                
+                className="google-Button-registration"
+                onClick={() => { console.log('Google button clicked') }}
+              />
+            </div>
           </MDBModalBody>
           {/* <MDBModalFooter>
             <MDBBtn color="secondary" onClick={this.toggle}>Close</MDBBtn>
