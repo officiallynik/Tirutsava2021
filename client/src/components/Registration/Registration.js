43c810efb7930701e5624372939360ca9150c018
Nikhil <nikhilownstheworld@gmail.com>
2021-01-08 13:24:06 +0530

    added registration to navbar

--- client/src/components/Registration/Registration.js
+++ client/src/components/Registration/Registration.js
@@ -1,49 +1,49 @@
 import React, { Component } from 'react';
-import GoogleButton from 'react-google-button'
-import "./Registration.css"
-import TirutsavaLogo from "./tirutsava.png"
+import GoogleButton from 'react-google-button';
+import "./Registration.css";
 import { MDBContainer, MDBBtn, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter } from 'mdbreact';
 
 class Registration extends Component {
-  state = {
-    modal: false
-  };
+	state = {
+		modal: false
+	};
 
-  toggle = () => {
-    this.setState({
-      modal: !this.state.modal
-    });
-  }
-  render() {
-    return (
-      <MDBContainer >
-        {/* BUTTON */}
-        <MDBBtn color="info" onClick={this.toggle}>Click</MDBBtn>
-        {/* MODAL */}
-        <MDBModal className="registration" isOpen={this.state.modal} toggle={this.toggle}    >
-          <MDBModalHeader toggle={this.toggle}>
-            
-            <img className="logo-registration" src={TirutsavaLogo}/>
-            {/* <h5 className="signIn-Heading-registration">Sign In to Tirutsava Brand</h5> */}
-            </MDBModalHeader>
-          <MDBModalBody>
-            <div className="modal-body-registration">
-              <p className="signIn-Heading-registration">Sign In to Tirutsava Brand</p>
-              <p className="secondary-Heading-registration">Log in with your Google account to access all the goodies!</p>
-              <GoogleButton
-                
-                className="google-Button-registration"
-                onClick={() => { console.log('Google button clicked') }}
-              />
-            </div>
-          </MDBModalBody>
-          {/* <MDBModalFooter>
+	toggle = () => {
+		this.setState({
+			modal: !this.state.modal
+		});
+	}
+	render() {
+		return (
+			<div>
+				<div onClick={this.toggle}>{this.props.btn}</div>
+				<MDBContainer >
+					{/* BUTTON */}
+					{/* MODAL */}
+					<MDBModal className="registration" isOpen={this.state.modal} toggle={this.toggle}    >
+						<MDBModalHeader toggle={this.toggle}>
+
+							<img className="logo-registration" src="/logo/tirutsava_big.png" />
+							{/* <h5 className="signIn-Heading-registration">Sign In to Tirutsava Brand</h5> */}
+						</MDBModalHeader>
+						<MDBModalBody>
+							<div className="modal-body-registration">
+								<p className="secondary-Heading-registration">Sign in with your Google account to access all the goodies!</p>
+								<GoogleButton
+
+									className="google-Button-registration"
+									onClick={() => { console.log('Google button clicked') }}
+								/>
+							</div>
+						</MDBModalBody>
+						{/* <MDBModalFooter>
             <MDBBtn color="secondary" onClick={this.toggle}>Close</MDBBtn>
             <MDBBtn color="primary">Save changes</MDBBtn>
           </MDBModalFooter> */}
-        </MDBModal>
-      </MDBContainer>
-    );
-  }
+					</MDBModal>
+				</MDBContainer>
+			</div>
+		);
+	}
 }
 export default Registration;
--- client/src/components/navbar/Toolbar.js
+++ client/src/components/navbar/Toolbar.js
@@ -1,48 +1,66 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Registration from "../Registration/Registration";
 import Burger from "./Burger";
 import "./Toolbar.css";
 
 const Toolbar = props => (
-  <div className="toolbar">
-    <nav className="toolbar__navigation">
+	<div className="toolbar">
+		<nav className="toolbar__navigation">
 
-      <div className="toolbar__logo">
-        <a className="navbar-brand" href="/">
-          <img
-            src="/logo/tirutsava_logo.png"
-            style={{ height: "100%", width: "324px" }}
-            alt="Tirutsava Logo"
-          />
-        </a>
-      </div>
+			<div className="toolbar__logo">
+				<a className="navbar-brand" href="/">
+					<img
+						src="/logo/tirutsava_logo.png"
+						style={{ height: "100%", width: "324px" }}
+						alt="Tirutsava Logo"
+					/>
+				</a>
+			</div>
 
-      <div className="toolbar_navigation-items">
-        <ul id="navit">
-          <li>
-            <Link to="/proshows">PROSHOWS</Link>
-          </li>
-          <li>
-            <Link to="/events">EVENTS</Link>
-          </li>
-          <li>
-            <Link to="/contactus">CONTACT US</Link>
-          </li>
-          <li>
-            <Link to="/sponsors">SPONSORS</Link>
-          </li>
-          <li>
-            <Link to="/faqs">FAQs</Link>
-          </li>
-        </ul>
-      </div>
+			<div className="toolbar_navigation-items hidden-nav">
+				<ul id="navit">
+					<li>
+						<Link to="/proshows">PROSHOWS</Link>
+					</li>
+					<li>
+						<Link to="/events">EVENTS</Link>
+					</li>
+					<li>
+						<Link to="/contactus">CONTACT US</Link>
+					</li>
+					<li>
+						<Link to="/sponsors">SPONSORS</Link>
+					</li>
+					<li>
+						<Link to="/faqs">FAQs</Link>
+					</li>
+					<Registration
+						btn={
+							<li>
+								<Link to="#">LOGIN</Link>
+							</li>
+						}
+					/>
+				</ul>
+			</div>
 
-      <div className="toolbar__toggle-button">
-        <Burger click={props.drawer} xbtn={props.xbtn} />
-      </div>
+			<div className="toolbar__toggle-button">
+				<Burger click={props.drawer} xbtn={props.xbtn} />
+			</div>
 
-    </nav>
-  </div>
+			<div className="login__toggle-button toolbar_navigation-items">
+				<Registration
+					btn={
+						<li>
+							<Link to="#">LOGIN</Link>
+						</li>
+					}
+				/>
+			</div>
+
+		</nav>
+	</div>
 );
 
 export default Toolbar;
