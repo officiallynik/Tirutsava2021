e2636de0c809330b4346990cb6c59fdd4af6c1e1
Talha-shamim <talhashamim001@gmail.com>
2021-01-09 14:43:17 +0530

    homepage phone

--- client/src/components/HomepagePhone/homepage.phone.js
+++ client/src/components/HomepagePhone/homepage.phone.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './homepage.phone.css';
 import Slider from 'react-slick';
+import Social from './social';
 var currentMode = ((new Date()).getHours() < 18 &&
 (new Date()).getHours() >= 6) ? 
 'day-mode' : 
@@ -9,35 +10,23 @@ var currentMode = ((new Date()).getHours() < 18 &&
 const HomepagePhone = () => {
     return ( 
         <React.Fragment>
-            <div className={`phone-body-${currentMode} row`}>
-            <div className="container-fluid">
+            <div className={`phone-body-${currentMode} conatainer-fluid`}>
                 <div className="row">
                     <div className="tirutsava-box col-md-12"><p className="tirutsava-title">Tirutsava</p></div>
                     <div className="col-md-12">
                         <div className="texts">text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic </div>
                     </div>
-                    <div className="btn-box btn"><button className="learn-more">Learn More</button></div>
+                    <div className="btn-box btn col-md-12"><button className="learn-more">Learn More</button></div>
 
-                    <div className="events-phone-box">
-                        <Slider speed={500} slidesToScroll={1} slidesToShow={1} infinite={true} autoplaySpeed={2000} autoplay={true} pauseOnHover={true} fade={true}>
-                            <img className="images-phone-slider" src="./headphone.png" alt="headphone pic"/>
-                            <img className="images-phone-slider" src="./technical2.png" alt="tech pic"/>
-                            <img className="images-phone-slider" src="./games.png" alt="online pic"/>
-                        </Slider>
+                    <div className="events-phone-box col-md-12">
+                    <Slider speed={500} slidesToScroll={1} slidesToShow={1} infinite={true} autoplaySpeed={2000} autoplay={true} pauseOnHover={true} fade={true}>
+                    <img className="images-phone-slider" src="./headphone.png" alt="headphone pic"/>
+                    <img className="images-phone-slider" src="./technical2.png" alt="tech pic"/>
+                    <img className="images-phone-slider" src="./games.png" alt="online pic"/>
+                    </Slider>
                     </div>
-                    
-                    {/*<div className="circle-wrapper">
-                        <div className="half-circle"></div> 
-                    </div>*/}
-
-                    <div className="social-media-phone">
-                        <i class="fa fa-facebook mr-2" aria-hidden="true"></i>
-                        <i class="fa fa-instagram mr-2" aria-hidden="true"></i>
-                        <i class="fa fa-twitter mr-2" aria-hidden="true"></i>
-                        <i class="fa fa-youtube mr-2" aria-hidden="true"></i>
-                    </div>
+                    <Social />
                 </div>
-            </div>
             </div>
         </React.Fragment>
      );
---
+++ client/src/components/HomepagePhone/social.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './social.css';
+
+const Social = () => {
+    return (    
+        <React.Fragment>
+            <div className="social-media-phone">
+                <i class="fa fa-facebook mr-2" aria-hidden="true"></i>
+                <i class="fa fa-instagram mr-2" aria-hidden="true"></i>
+                <i class="fa fa-twitter mr-2" aria-hidden="true"></i>
+                <i class="fa fa-youtube mr-2" aria-hidden="true"></i>
+            </div>
+        </React.Fragment>
+      );
+}
+ 
+export default Social;  
