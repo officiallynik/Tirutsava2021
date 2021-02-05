f5e30a0282224404d591719fb2fe5bda175eb7b7
Nikhil <nikhilownstheworld@gmail.com>
2021-02-05 22:52:24 +0530

    added more event posters and rules

--- client/src/pages/Merch/Form.js
+++ client/src/pages/Merch/Form.js
@@ -9,57 +9,11 @@ const MerchForm = props => {
                 <MDBCol md="8">
                     <MDBCard>
                         <MDBCardBody>
-                            <form>
-                                <p className="h4 text-center py-4">Buy our awesome merch</p>
-                                <label htmlFor="defaultFormContactMessageEx" className="grey-text">
-                                    Hoodie Size
-                                </label>
-                                <select className="browser-default custom-select" style={{ height: "100%", fontFamily: "sans-serif" }}
-                                    onChange={e => {
-                                        if (e.target.value === "Size") {
-                                            
-                                        }
-                                        else {
-                                            props.setHoodieSize(e.target.value)
-                                        }
-                                    }}
-                                >
-                                    <option value={null}>Size</option>
-                                    <option value="XS">XS</option>
-                                    <option value="S">S</option>
-                                    <option value="M">M</option>
-                                    <option value="L">L</option>
-                                    <option value="XL">XL</option>
-                                    <option value="XXL">XXL</option>
-                                </select>
-                                <div style={{textAlign: "left"}}>
-                                    <a href="/merch/size_chart.jpeg" target = "_blank" rel = "noopener noreferrer">Size chart for reference</a>
-                                </div>
-
-                                <label htmlFor="defaultFormContactMessageEx" className="grey-text">
-                                    Delivery Address
-                                </label>
-                                <textarea type="text" id="defaultFormContactMessageEx" className="form-control" rows="3" 
-                                    onChange={e => props.setAddr(e.target.value)}
-                                />
-
-                                <label htmlFor="defaultFormLoginPincode" className="grey-text">
-                                Pincode
-                                </label>
-                                <input type="email" id="defaultFormLoginPincode" className="form-control" 
-                                    placeholder="Ex. 560001"
-                                    onChange={e => props.setPincode(e.target.value)}
-                                />
-
-                                <div className="text-center py-4 mt-3">
-                                    <MDBBtn className="btn btn-outline-purple" 
-                                        onClick={props.handleConfirmation}
-                                        disabled={props.disabled}
-                                    >
-                                        Buy Now
-                                    </MDBBtn>
-                                </div>
-                            </form>
+                            <div
+                                style={{display: "flex", justifyContent: "center", alignItems: "center", color: "black"}}
+                            >   
+                                No longer available
+                            </div>
                         </MDBCardBody>
                     </MDBCard>
                 </MDBCol>
